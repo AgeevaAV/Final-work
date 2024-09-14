23459e0ced29058fbe6cb99b8d89a1740e8f7933
Anastasiia <nasaageeva8052@gmail.com>
2024-09-14 13:26:22 +0300

    задаю начальные значения

---
+++ Desktop/02_CHARP/Program.cs
@@ -0,0 +1,93 @@
+﻿// Console.Write("Введите кол-во строк: ");
+// int rows= Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите кол-во столбцов: ");
+// int cols= Convert.ToInt32(Console.ReadLine());
+// int[,] matrix=new int[rows, cols];
+// Random rand = new Random();
+// for (int i=0; i<matrix.GetLength(0); i++){
+//     for (int j=0; j<matrix.GetLength(1); j++){
+//         matrix[i,j]= rand.Next(0,10);
+//     }
+// }
+// for (int i=0; i<matrix.GetLength(0); i++){
+//     for (int j=0; j<matrix.GetLength(1); j++){
+//        if(i%2==0 && j%2==0){
+//         matrix[i,j] *= matrix[i,j];
+//        }
+//     }
+// }
+// for (int i=0; i<matrix.GetLength(0); i++){
+//     for (int j=0; j<matrix.GetLength(1); j++){
+//         Console.Write(matrix[i,j] + " ");
+//     }
+//     Console.WriteLine();
+// }
+// индекса чётные, и замените эти элементы на их квадраты.
+// Пример
+// 2 3 4 3 4 3 4 3
+// 4 3 4 1 => 4 3 4 1
+// 2 9 5 4 2 9 25 4
+
+void Main()
+{
+int rows = ReadInt("Введите количество строк: ");
+int cols = ReadInt("Введите количество столбцов: ");
+
+int[,] matrix = GenerateMatrix(rows, cols, 0, 9);
+
+PrintMatrix(matrix);
+
+ChangeMatrix(matrix);
+
+System.Console.WriteLine();
+
+PrintMatrix(matrix);
+
+}
+
+void ChangeMatrix(int[,] oldMatrix)
+{
+for (int i = 0; i < oldMatrix.GetLength(0); i += 2)
+{
+for (int j = 0; j < oldMatrix.GetLength(1); j += 2)
+{
+oldMatrix[i, j] *= oldMatrix[i, j];
+}
+}
+}
+
+void PrintMatrix(int[,] arrayForPrint)
+{
+for (int i = 0; i < arrayForPrint.GetLength(0); i++)
+{
+for (int j = 0; j < arrayForPrint.GetLength(1); j++)
+{
+System.Console.Write(arrayForPrint[i, j] + "\t");
+}
+System.Console.WriteLine();
+}
+}
+
+int[,] GenerateMatrix(int row, int col, int leftRange, int rightRange)
+{
+int[,] tempMatrix = new int[row, col];
+Random rand = new Random();
+
+for (int i = 0; i < row; i++)
+{
+for (int j = 0; j < col; j++)
+{
+tempMatrix[i, j] = rand.Next(leftRange, rightRange + 1);
+}
+}
+
+return tempMatrix;
+}
+
+int ReadInt(string msg)
+{
+System.Console.Write(msg);
+return Convert.ToInt32(Console.ReadLine());
+}
+
+Main();
---
+++ Desktop/all_C#/ceminar_C#_2/Program.cs
@@ -0,0 +1,146 @@
+﻿// void Main(){
+//     int sizeArray = ReadInt("введите размер массива: ");
+//     int[] array = GeneratorArray(sizeArray, 0, 30);
+//     PrintArray(array);
+//     Console.WriteLine(CountPrimes(array));
+//     // Console.WriteLine(CountNumbers(array, 7, 1));
+// }
+// bool isPrime(int number){
+//     if (number<2) return false;
+//     for(int i=2; i<=Math.Sqrt(number); i++){
+//         if(number %i ==0)
+//             return false; }
+//     return true;
+// }
+// int CountPrimes ( int[] array){
+//     int count =0;
+//     foreach (int number in array){
+//     if(isPrime(number)){
+//         count++;
+//     }
+//     }
+//     return count;
+// }
+// int ReadInt(string msg)
+// {
+//     System.Console.Write(msg);
+//     return Convert.ToInt32(Console.ReadLine());
+// }
+// int[] GeneratorArray(int size, int leftRange, int rightRange){
+//     int[] tempArray = new int[size];
+//     Random rand = new Random();
+//     for (int i =0; i<size; i++){
+//         tempArray[i]=rand.Next(leftRange,rightRange+1);
+// }
+//     return tempArray;
+// }
+// void PrintArray(int[] arrayForPrint)
+// {
+//     System.Console.WriteLine("[ " + string.Join(", ", arrayForPrint) + " ]");
+// }
+// Main();
+
+// задача первая
+
+// void Main()
+// {
+// while (true) 
+// {
+//     int sum = 0;
+//     Console.Write("Введите число или 'q' для выхода: ");
+//     string input = Console.ReadLine(); 
+//     if (input == "q") 
+// {
+//     break;
+// }
+// int number;
+// if (int.TryParse(input, out number)) 
+//     while (number > 0) 
+// {
+//     sum += number % 10; 
+// number /= 10; 
+// }
+// if (sum % 2 == 0) 
+// {
+// Console.WriteLine("[STOP]");
+// break;
+// }
+// else 
+// {
+// Console.WriteLine("Некорректный ввод. Пожалуйста, введите целое числоили 'q'.");
+// }
+// }
+// }
+// Main();
+
+//задача вторая
+// void Main()
+// {
+//     int sizeArray=ReadInt("введите размер массива: ");
+//     int[] numbers = new int[sizeArray]; 
+//     Random random = new Random(); 
+//     int evenCount = 0; 
+//     for (int i = 0; i < numbers.Length; i++)
+// {
+//         numbers[i] = random.Next(100, 1000); 
+//         Console.Write(numbers[i] + " "); 
+
+//     if (numbers[i] % 2 == 0)
+// {
+//     evenCount++;
+// }
+// }
+
+// Console.WriteLine($"\nКоличество четных чисел в массиве: {evenCount}");
+// }
+// Main();
+// int ReadInt(string msg)
+// {
+//     System.Console.Write(msg);
+//     return Convert.ToInt32(Console.ReadLine());
+// }
+
+// задач третья 
+void Main()
+{
+int sizeArray = ReadInt("введите размер массива: ");
+int[] array = GeneratorArray(sizeArray, 0, 30);
+PrintArray(array);
+int temp;
+Console.Write("Исходный массив: ");
+foreach (int number in array)
+{
+Console.Write(number + " ");
+}
+
+for (int i = 0; i < array.Length / 2; i++)
+{
+
+    temp = array[i];
+    array[i] = array[array.Length - 1 - i];
+    array[array.Length - 1 - i] = temp;
+}
+Console.Write("\nПеревернутый массив: ");
+foreach (int number in array)
+{
+    Console.Write(number + " ");
+}
+}
+int ReadInt(string msg)
+{
+    System.Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int[] GeneratorArray(int size, int leftRange, int rightRange){
+    int[] tempArray = new int[size];
+    Random rand = new Random();
+    for (int i =0; i<size; i++){
+        tempArray[i]=rand.Next(leftRange,rightRange+1);
+}
+    return tempArray;
+}
+void PrintArray(int[] arrayForPrint)
+{
+    System.Console.WriteLine("[ " + string.Join(", ", arrayForPrint) + " ]");
+}
+Main();
